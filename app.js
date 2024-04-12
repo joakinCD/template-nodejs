cf2c97c0cd2b74eed007a9dde15c51dd8e844355
joaquin-gopick <joakinchaparro@hotmail.com>
2024-04-12 10:46:15 +0200

    primeros cambios

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const port = process.env.PORT ?? 3000;
 
 app.use(express.static('public'))
 
-app.get('*', (req, res) => {
-    res.redirect('/');
-})
 
+app.get('/pruebaGet', (req, res) => {
+    res.send('hola');
+})
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 })
