cf0a0c15eacfb8cdcf30863469484c4b89692af6
joaquin-gopick <joakinchaparro@hotmail.com>
2024-04-12 11:34:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,18 @@ const port = process.env.PORT ?? 3000;
 
 app.use(express.static('public'))
 
-
+app.get('/login', (req, res) => {
+  var email = req.body.email
+  var pass = req.body.pass
+  console.log(email,pass)
+  var query = connection.query('SELECT * FROM usuarios WHERE email="'+email+'" AND pass="'+pass+'"', function(error, result){
+       if(error){
+          throw error;
+       }else{
+          res.status(200).json({ datos : result[0]})
+       }
+  });
+})
 app.get('/pruebaGet', (req, res) => {
     res.send('hola');
 })
